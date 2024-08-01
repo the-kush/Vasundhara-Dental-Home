3d20c51f2b9caba097f1e5ccebeebc756e732a67
Kush <kushsinghdsc@gmail.com>
2024-08-01 13:50:26 +0530

    clicking feature added to whatsappbooking

--- src/components/WhatsappBooking.jsx
+++ src/components/WhatsappBooking.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { FaWhatsapp } from 'react-icons/fa';
 
 const WhatsappBooking = () => {
+
+  const handleClick = () => {
+    window.location.href = 'https://wa.link/090sm7'
+  }
+
   return (
     <div className="fixed bottom-4 right-4 flex items-center p-2 shadow-lg rounded-lg">
       <a href="https://wa.link/090sm7" className='hover:text-black-500'><FaWhatsapp className='text-6xl text-green-600'/></a>
-      <button className=" text-black py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300 text-2xl">
+      <button onClick={handleClick} className=" text-black py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300 text-2xl">
         Book an Appointment Now
       </button>
     </div>
