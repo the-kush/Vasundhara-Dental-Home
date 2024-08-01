4f659da909b4711612479e39bfe32f5d5678aa1e
Kush <kushsinghdsc@gmail.com>
2024-08-01 11:09:11 +0530

    completed all pages for smaller screens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,32 @@ import { FaFacebook, FaInstagram } from 'react-icons/fa6';
 const Footer = () => {
   return (
     <div className='!px-0 !py-10 bg-purple-200 font-serif'>
-        <div className='container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col h-10'>
-            <p className='caption texr-n-4 lg:block ml-32'> © {new Date().getFullYear()} All rights reserved.</p>
-            <p className='text-xl hover:subpixel-antialiased font-semibold'>Vasundhara Dental Home</p>
-            <ul className='flex gap-5 flex-wrap'>
-                <a href="" className='hover:text-black-500'><FaFacebook /></a>
-                <a href="https://www.instagram.com/vasundhara_dental_home?igsh=MWxmYzBjdHF1cGI3MA==" className='hover:text-black-500'><FaInstagram /></a>
-                <a href="https://wa.link/090sm7" className='hover:text-black-500'><FaWhatsapp /></a>
-                <a href="https://www.google.com/maps/dir//vasundhara+dental+home/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x390ce56728aaf793:0xa7ebca477d53b386?sa=X&ved=1t:3061&ictx=111"><FaMapMarkedAlt /></a>
-                <a href=""><FaPhoneAlt /></a>
-                <a href=""><MdMail /></a>
-            </ul>
-        </div>
+      <div className='container flex flex-col sm:flex-row sm:justify-between items-center gap-10 sm:h-20'>
+        <p className='caption text-n-4 sm:ml-32'>© {new Date().getFullYear()} All rights reserved.</p>
+        <p className='text-xl font-semibold'>Vasundhara Dental Home</p>
+        <ul className='flex gap-5 flex-wrap justify-center'>
+          <li>
+            <a href="" className='hover:text-black-500'><FaFacebook /></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/vasundhara_dental_home?igsh=MWxmYzBjdHF1cGI3MA==" className='hover:text-black-500'><FaInstagram /></a>
+          </li>
+          <li>
+            <a href="https://wa.link/090sm7" className='hover:text-black-500'><FaWhatsapp /></a>
+          </li>
+          <li>
+            <a href="https://www.google.com/maps/dir//vasundhara+dental+home/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x390ce56728aaf793:0xa7ebca477d53b386?sa=X&ved=1t:3061&ictx=111"><FaMapMarkedAlt /></a>
+          </li>
+          <li>
+            <a href=""><FaPhoneAlt /></a>
+          </li>
+          <li>
+            <a href=""><MdMail /></a>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer
--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -7,27 +7,29 @@ import WhatsappBooking from '../components/WhatsappBooking';
 const ContactUs = () => {
   return (
     <div className="font-serif">
-      <div className="p-12 pl-24 pr-24 pb-36">
-        <h1 className="text-6xl font-bold text-center mb-10 text-orange-300">Contact Us</h1>
-        <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="p-4 md:p-12 md:pl-24 md:pr-24 pb-36">
+        <h1 className="text-4xl md:text-6xl font-bold text-center mb-10 text-orange-300">Contact Us</h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           <div className="space-y-8">
             <div className="flex items-center space-x-4">
               <FaPhone className="text-blue-500" size={36} />
-              <span className="text-xl pl-20">+91 9990277066</span>
+              <span className="text-lg p-5 md:text-xl">+91 9990277066</span>
             </div>
             <div className="flex items-center space-x-4">
               <FaEnvelope className="text-green-500" size={36} />
-              <span className="text-xl pl-20">dr.kopalsingh21@gmail.com</span>
+              <span className="text-lg p-5 md:text-xl">dr.kopalsingh21@gmail.com</span>
             </div>
             <div className="flex items-center space-x-4">
               <FaMapMarkerAlt className="text-red-500" size={36} />
-              <span className="text-xl pl-20">E-333/4, Khosla Market, <br />
-              Opp Mahesh Apt, <br />
-              New Ashok Nagar, Delhi - 110096</span>
+              <span className="text-lg p-5 md:text-xl">
+                E-333/4, Khosla Market, <br />
+                Opp Mahesh Apt, <br />
+                New Ashok Nagar, Delhi - 110096
+              </span>
             </div>
             <div className="flex items-center space-x-4">
               <FaThumbsUp className="text-yellow-500" size={36} />
-              <span className="text-lg flex space-x-4 pl-20">
+              <span className="text-lg p-5 flex space-x-4">
                 <a href="https://www.facebook.com/profile.php?id=61559639683181&mibextid=ZbWKwL" className='hover:text-black-500'><FaFacebook className='text-2xl text-blue-500'/></a>
                 <a href="https://www.instagram.com/vasundhara_dental_home?igsh=MWxmYzBjdHF1cGI3MA==" className='hover:text-black-500'><FaInstagram className='text-2xl text-pink-600'/></a>
               </span>
@@ -35,20 +37,23 @@ const ContactUs = () => {
           </div>
 
           <div className="flex items-center justify-center">
-            <img
-              src={loc}
-              alt="https://www.google.com/maps/dir//vasundhara+dental+home/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x390ce56728aaf793:0xa7ebca477d53b386?sa=X&ved=1t:3061&ictx=111"
-              className="max-w-full h-auto rounded shadow-md"
-            />
+            <a href="https://www.google.com/maps/dir//vasundhara+dental+home/data=!4m6!4m5!1m1!4e2!1m2!1m1!1s0x390ce56728aaf793:0xa7ebca477d53b386?sa=X&ved=1t:3061&ictx=111" target="_blank" rel="noopener noreferrer">
+              <img
+                src={loc}
+                alt="Location Map"
+                className="max-w-full h-auto rounded shadow-md cursor-pointer"
+              />
+            </a>
           </div>
         </div>
       </div>
-      <div className='text-center bg-red-200'><h4 className='text-2xl'>*<u>NOTE</u>*</h4>
-      <p className='text-xl'>Bookings are done mainly through WhatsApp. It can also be done by calling on the phone number or visiting the clinic.</p>
+      <div className='text-center bg-red-200 p-8'>
+        <h4 className='text-2xl'><u>NOTE</u></h4>
+        <p className='text-xl'>Bookings are done mainly through WhatsApp. It can also be done by calling on the phone number or visiting the clinic.</p>
       </div>
       <WhatsappBooking />
     </div>
   )
 }
 
-export default ContactUs
+export default ContactUs;
