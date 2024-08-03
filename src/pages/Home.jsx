a76ddb342298eedc141c12d4966b0354296b9f34
Kush <kushsinghdsc@gmail.com>
2024-08-03 11:35:39 +0530

    did a little seo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Helmet } from 'react-helmet'
 import ImageSlidder from '../components/ImageSlidder'
 import KopalSingh from '../components/KopalSingh'
 import HomeFacilities from '../components/HomeFacilities'
@@ -9,6 +10,11 @@ import SocialMedia from '../components/SocialMedia'
 const Home = () => {
   return (
     <>
+    <Helmet>
+    <title>Home Page</title>
+        <meta name="description" content="This is official home page for Vasundhara Dental Home. Dental services provided at the best price & doctor makes sure you have a best environment for you & your family." />
+        <meta name="keywords" content="Dentist, dental, vasundhara dental home, vasundhara, dental home, tooth" />
+    </Helmet>
     <div className='container mx-auto sm:text-center'>
       <ImageSlidder />
     </div>
